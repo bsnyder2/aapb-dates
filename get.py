5f8f321e2a15edefa598549c6328319467ef64a8
Ben Snyder <bsnyder2@users.noreply.github.com>
2024-08-03 17:51:42 -0400

    Cleaned up and todos

--- get.py
+++ get.py
@@ -1,9 +1,12 @@
 import requests
-import lxml
+from requests_cache import CachedSession
 from bs4 import BeautifulSoup
+
+import lxml
 import time
 
 
+# Simple timer
 class Timer:
     def __init__(self):
         self.start_time = 0
@@ -19,53 +22,34 @@ class Timer:
         return self.end_time - self.start_time
 
 
-from requests_cache import CachedSession
-
-# DNS caching is 300x faster
-session = CachedSession(expire_after=600)
-
 per_page = 100
 
-foo = f"https://americanarchive.org/catalog?f[access_types][]=online&per_page={per_page}&sort=asset_date+asc"
+# DNS caching is 300x faster nice
+session = CachedSession(expire_after=600)
+# timer = Timer()
+# timer.start()
 
 
-bar = requests.get(foo)
-baz = BeautifulSoup(bar.text, "html.parser")
-rows = baz.find(class_="documents-list")
-
-
-timer = Timer()
-timer.start()
-
-docs = []
-
-# how fast?
-
-
-# docs = [row.find("a")["href"] for row in rows.find_all(class_="document")]
-# bar = current.find_all(class_="document")
-
-# print(bar[0])
-
-# docs2 = []
-# print(len(docs))
-
-for row in rows.find_all(class_="document"):
-    record_link = row.find("a")["href"]
-    response = session.get(f"https://americanarchive.org{record_link}.pbcore")
-    print(response.status_code)
-
-# docs3 = []
-
-timer.end()
-print(timer.elapsed())
-
-# Iterate through documents-list
+# TODO: while records are dated- all pages
 
 # Sorting by oldest ensures that all dated records appear first
 # Count up how many dated records there are = ((per_page * "fully dated pages") + "dated on last page") / "available online"
+first_page = f"https://americanarchive.org/catalog?f[access_types][]=online&per_page={per_page}&sort=asset_date+asc"
 
-# start = time.time()
-# print(start)
-# zz = requests.get("https://americanarchive.org/catalog/cpb-aacip_80-644qs701.pbcore")
-# print()
+
+page_response = requests.get(first_page)
+page_soup = BeautifulSoup(page_response.text, "html.parser")
+rows = page_soup.find(class_="documents-list")
+
+
+# Iterate through records for links
+for row in rows.find_all(class_="document"):
+    record_link = row.find("a")["href"]
+    # possible to do xml request instead of http?
+    xml_response = session.get(f"https://americanarchive.org{record_link}.pbcore")
+    print(xml_response.status_code)
+    # ...
+
+
+# timer.end()
+# print(timer.elapsed())
