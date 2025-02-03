97eb88e38169a742fb856856ef0095d22e9cd6e3
Ben Snyder <bsnyder2@users.noreply.github.com>
2025-02-03 15:16:11 -0500

    script cleanup

--- counter.py
+++ counter.py
@@ -1,19 +1,7 @@
 file = open("./data-2024-08/years-all.tsv")
-
 sum = 0
-
-lst = ["2020", "2021", "2022", "2023", "2024"]
-# lst2 = ["1995", "1996", "1997", "1998", "1999"]
-
-
 for line in file:
     if int(line[-5:-1]) == 2011:
         sum += 1
 
 print(sum)
-
-# 169 records before 1940
-
-# TODO issue: online archive shows fewer records in x year than retrieved from api, e.g. 1992
-# is this an issue?
-# Actually, implication of this is that I don't know how many total undated records there are. But honestly it's fine
--- visual.py
+++ visual.py
@@ -14,7 +14,6 @@ filepaths = [
     "./data-2024-08/years-digitized.tsv",
     "./data-2024-08/years-online.tsv",
 ]
-# lastyear = 0
 hist_datas = [[], [], []]
 for hist_data_i, filepath in enumerate(filepaths):
     for line in open(filepath, "r"):
@@ -22,25 +21,11 @@ for hist_data_i, filepath in enumerate(filepaths):
         if year < START_YEAR:
             continue
         hist_datas[hist_data_i].append(year)
-        # if year == 2023 and hist_data_i == 0:
-        # lastyear += 1
-# print(lastyear)
-
-print(hist_datas[2])
-c = 0
-for d in hist_datas[2]:
-    if d == 2022:
-        c += 1
-print(c)
-
 
 bins1 = np.arange(START_YEAR, 2021, BIN_WIDTH)
 bins2 = np.arange(2021, 2026, BIN_WIDTH)
 bins = np.concatenate((bins1, bins2)) if BIN_WIDTH <= 4 else np.append(bins1, 2026)
 ticks = np.arange(START_YEAR, 2030, 10)
-print(bins)
-
-print(bins)
 
 labels = ["All Records", "All Digitized", "Available Online"]
 
@@ -49,8 +34,6 @@ ax.set_xticks(ticks)
 for i, data in enumerate(hist_datas):
     ax.hist(data, bins, label=labels[i])
 ax.legend()
-
-# how many from 2024? negligible? right now only includes up to 2023. so first after each tick is the decade mark
 
 # decade bins
 ax.set_ylim(0, 170000)
